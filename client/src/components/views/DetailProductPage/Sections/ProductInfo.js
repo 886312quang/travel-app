7836e4bb5fda394c06880e2358694315757ca9b5
QuangPham <886312quangpham@gmail.com>
2020-08-04 22:44:15 +0700

    add-to-card

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -8,6 +8,10 @@ function ProductInfo(props) {
     setProduct(props.detail);
   }, [props.detail]);
 
+  const addToCartHandler = () => {
+    props.addToCart(props.detail._id);
+  };
+
   return (
     <div>
       <Descriptions title="Product Info">
@@ -22,7 +26,12 @@ function ProductInfo(props) {
       <br />
       <br />
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <Button size="large" shape="round" type="danger" onClick>
+        <Button
+          size="large"
+          shape="round"
+          type="danger"
+          onClick={addToCartHandler}
+        >
           Add to cart
         </Button>
       </div>
